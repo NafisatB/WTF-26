5d29875e85ae653fba2c2aadc459d1b647664c05
Nafisat <nafisatbabamusa@outlook.com>
2025-09-06 16:51:20 +0100

    Quiz_1

---
+++ QUIZ_WK1/quiz.js
@@ -0,0 +1,32 @@
+//      QUIZ-WK 1
+
+//Declare three variables: num1, num2, and name. 
+ 
+ //Assign number values to num1 and num2, and a string value to name. 
+ 
+// Perform and print the results of: 
+ 
+ //Addition (num1 + num2) 
+ 
+ //Subtraction (num1 - num2) 
+ 
+ //Multiplication (num1 * num2) 
+ 
+ //Division (num1 / num2) 
+ 
+ 
+ 
+ //Print a message:
+ //"Hello [name], the result of num1 + num2 is [result]"
+//push to github and enter the repository link
+
+//      SOLUTION
+
+let num1 = 2;
+let num2 = 3;
+let name = 'Jone';
+
+alert(num1 + num2);
+alert(num1 - num2);
+alert(num1 * num2);
+alert("Hello " + name + ", the result of " + num1 + " + " + num2 + " is " + (num1 + num2) );
