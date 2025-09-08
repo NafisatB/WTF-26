70e565e59f99c48c2d64957d4d6683c1e6d590e5
Nafisat <nafisatbabamusa@outlook.com>
2025-09-08 10:29:45 +0100

    all classes

---
+++ WK1_Class/javascript.js
@@ -0,0 +1,9 @@
+// const name = prompt("What is your name?")
+// console.log(name)
+
+// let jobTitle = prompt("What is your job role?")
+// console.log(jobTitle)
+
+const name = "John";
+
+console.log(`Hello ${name}`);
--- javascript.js
+++
@@ -1,6 +0,0 @@
-const name = prompt("What is your name?")
-console.log(name)
-
-let jobTitle = prompt("What is your job role?")
-console.log(jobTitle)
-
